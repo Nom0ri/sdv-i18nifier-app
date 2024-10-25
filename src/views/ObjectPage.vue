03cb85036e7db251966cbdfd3af512fb9db7e4d8
Arkadiusz Kaproń <56255684+Nom0ri@users.noreply.github.com>
2024-10-25 23:34:17 +0200

    fix: {{ModID}} token handling for objects

--- src/views/ObjectPage.vue
+++ src/views/ObjectPage.vue
@@ -9,13 +9,12 @@
         @update:inputText="inputText = $event"
         @update:customToken="customToken = $event"
     />
-  </template>
-  
-  
-  <script>
-  import PageLayout from '../components/PageLayout.vue';
-  
-  export default {
+</template>
+
+<script>
+import PageLayout from '../components/PageLayout.vue';
+
+export default {
     components: { PageLayout },
 
     data() {
@@ -42,33 +41,33 @@
 
     methods: {
         checkInput(inputString) {
-            const matches = inputString.match(/\"([^"]+)\":\s*{[^}]*\s*"DisplayName":\s*"([^"]*)",?\s*"Description":\s*"([^"]*)"/g);
+            const sanitizedInput = inputString.replace(/{{ModId}}_/g, '');
+            const matches = sanitizedInput.match(/"([^"]+)":\s*{[^}]*\s*"DisplayName":\s*"([^"]*)",?\s*"Description":\s*"([^"]*)"/g);
 
             if (!matches || matches.length === 0) {
                 this.contentText = 'Incorrect format';
                 this.i18nText = 'Input needs to have at least this info:\n"<ID>": {\n \t\t ...\n \t\t "DisplayName": "<DisplayName>",\n \t\t "Description": "<Description>", \n \t\t ... \n\t\t}';
                 return false;
-                } else {
-                    return true;
-                }
+            } else {
+                return true;
+            }
         },
 
         processData(inputString) {
-            const rawData = this.inputText;
-            const isValidInput = this.checkInput(rawData);
+            const sanitizedInput = inputString.replace(/{{ModId}}_/g, '').replace(/}}/g, '');
+            const isValidInput = this.checkInput(sanitizedInput);
 
             if (!isValidInput) {
                 return;
             }
 
-            const matches = inputString.match(/\"([^"]+)\":\s*{[^}]*\s*"DisplayName":\s*"([^"]*)",?\s*"Description":\s*"([^"]*)"/g);
-
+            const matches = sanitizedInput.match(/"([^"]+)":\s*{[^}]*\s*"DisplayName":\s*"([^"]*)",?\s*"Description":\s*"([^"]*)"/g);
             let i18nText = '';
             let contentText = inputString;
 
             if (matches) {
                 matches.forEach(match => {
-                    const [, id, displayName, description] = match.match(/\"([^"]+)\":\s*{[^}]*\s*"DisplayName":\s*"([^"]*)",?\s*"Description":\s*"([^"]*)"/);
+                    const [, id, displayName, description] = match.match(/"([^"]+)":\s*{[^}]*\s*"DisplayName":\s*"([^"]*)",?\s*"Description":\s*"([^"]*)"/);
 
                     const nameToken = `${this.customToken.length > 0 ? this.customToken + '.' : ''}${id}.DisplayName`;
                     const descriptionToken = `${this.customToken.length > 0 ? this.customToken + '.' : ''}${id}.Description`;
@@ -76,22 +75,18 @@
                     i18nText += `"${nameToken}": "${displayName}",\n`;
                     i18nText += `"${descriptionToken}": "${description}",\n`;
 
-                    var escapedDisplayName = this.escapeRegExp(displayName)
-                    var escapedDescription = this.escapeRegExp(description)
+                    const escapedDisplayName = this.escapeRegExp(displayName);
+                    const escapedDescription = this.escapeRegExp(description);
 
-                    var displayNameRegex = new RegExp(`"DisplayName":\\s*("${escapedDisplayName}")`);
-                    var descriptionRegex = new RegExp(`"Description":\\s*("${escapedDescription}")`);
+                    const displayNameRegex = new RegExp(`"DisplayName":\\s*("${escapedDisplayName}")`);
+                    const descriptionRegex = new RegExp(`"Description":\\s*("${escapedDescription}")`);
 
                     contentText = contentText.replace(displayNameRegex, `"DisplayName": "{{i18n:${nameToken}}}"`);
                     contentText = contentText.replace(descriptionRegex, `"Description": "{{i18n:${descriptionToken}}}"`);
                 });
             }
 
-            if (i18nText.trim() !== '') {
-                this.contentText = contentText.trim() + `\n`;
-            } else {
-                this.contentText = '';
-            }
+            this.contentText = i18nText.trim() ? `${contentText}\n` : '';
             this.i18nText = i18nText.trim();
         },
 
